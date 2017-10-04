67aee4bcd15aced5f43ebaf7b754fb3cb90b20f4
Leslie Alvarado <lesliemalvarado@gmail.com>
2017-10-04 10:34:52 -0400

    first commit

---
+++ app/models/blog_post.rb
@@ -0,0 +1,13 @@
+class BlogPost < ApplicationRecord
+
+	has_many :comments, dependent: :destroy
+
+	validates :title, :blog_entry, presence: true
+	validates :title, length: { maximum: 140 }
+
+
+
+
+
+
+end
